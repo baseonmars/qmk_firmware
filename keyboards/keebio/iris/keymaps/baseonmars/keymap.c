1bc36a2ce0fb81a073df29cefc4d99037b59a7ae
Dan Cseko <dan@nate.tech>
2021-11-21 14:26:22 +0000

    feat: attempt to use layers more

--- keyboards/keebio/iris/keymaps/baseonmars/keymap.c
+++ keyboards/keebio/iris/keymaps/baseonmars/keymap.c
@@ -14,9 +14,6 @@ enum custom_keycodes {
   ADJUST,
 };
 
-
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
@@ -27,23 +24,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LGUI,           KC_RGUI,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPACE,           KC_ENT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LALT, LOWER,   KC_SPACE,                    KC_ENT,  RAISE,   KC_RALT
+                                    KC_LALT, LOWER,   KC_SPACE,                    KC_ENT,  RAISE,   KC_LGUI
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_GRAVE,  KC_P7,   KC_P8,   KC_P9,  _______,  _______,                            _______, _______, _______, KC_MINS, KC_EQL, KC_DEL,
+     KC_NUBS,  KC_P7,   KC_P8,   KC_P9,  KC_KP_ASTERISK,  _______,                            _______, _______, _______, KC_MINS, KC_EQL, KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_CAPSLOCK,   KC_P4,   KC_P5,   KC_P6,   _______,    _______,                               _______, KC_LCBR, KC_RCBR, KC_LBRACKET, KC_RBRACKET, KC_PGUP,
+     KC_CAPSLOCK,   KC_P4,   KC_P5,   KC_P6,   KC_KP_SLASH,    _______,                               _______, KC_LCBR, KC_RCBR, KC_LBRACKET, KC_RBRACKET, KC_PGUP,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT,  KC_P1,   KC_P2,   KC_P3,   _______,   _______,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_HOME, KC_PGDOWN,
+     KC_LSFT,  KC_P1,   KC_P2,   KC_P3,   KC_KP_PLUS,   _______,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_HOME, KC_PGDOWN,
   //├────────┼────────┼────────┼────────┼────────┼───────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_GRAVE, KC_P0,   KC_DEL,   KC_P3,  _______, _______, KC_LGUI,          KC_RGUI, _______, _______,   _______,   _______,   KC_END, KC_RSHIFT,
+     KC_LCTL, KC_NUMLOCK,   KC_KP_0,   KC_KP_DOT,  KC_KP_MINUS, _______, KC_SPACE,          KC_ENTER, _______, _______,   _______,   _______,   KC_END, KC_RSHIFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LALT, _______, KC_SPACE,                    KC_ENTER,  ADJUST, KC_LALT
+                                    KC_LALT, _______, KC_SPACE,                    KC_ENTER,  ADJUST, KC_LGUI
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -51,13 +48,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_TOG, _______, _______,   _______, _______,  _______,                            _______, _______, _______, _______, KC_PSCREEN, _______,
+     RGB_TOG, _______, _______,   _______, KC_LCBR,  KC_RCBR,                            _______, _______, _______, _______, KC_PSCREEN, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_MOD, KC_MPRV, KC_MNXT, KC_VOLU, _______, _______,                            _______,  _______, RGB_HUI, RGB_SAI, RGB_VAI, _______,
+     RGB_MOD, _______, _______, _______, KC_LBRC, KC_RBRC,                            RGB_HUI,  RGB_SAI, RGB_VAI, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, _______, _______, KC_LGUI,          KC_RGUI, _______,  _______,  RGB_HUD, RGB_SAD, RGB_VAD, _______,
+     KC_LCTL, _______, _______, _______, KC_LPRN, KC_RPRN, KC_SPACE,          KC_ENTER, RGB_HUD,  RGB_SAD,  RGB_VAD, _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LALT, ADJUST, KC_SPACE,                   KC_ENTER, _______, KC_LALT
+                                    KC_LALT, ADJUST, KC_SPACE,                   KC_ENTER, _______, KC_LGUI
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -133,3 +130,115 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     }
     return true;
 }
+
+
+/*
+"vizsymbols": {
+        "_______": "     ",
+        "KC_TRANSPARENT": "_____",
+        "KC_TRANS": "     ",
+        "KC_0": "  0  " ,
+        "KC_1": "  1  " ,
+        "KC_2": "  2  " ,
+        "KC_3": "  3  " ,
+        "KC_4": "  4  " ,
+        "KC_5": "  5  " ,
+        "KC_6": "  6  " ,
+        "KC_7": "  7  " ,
+        "KC_8": "  8  " ,
+        "KC_9": "  9  " ,
+        "KC_A": "  A  " ,
+        "KC_B": "  B  " ,
+        "KC_C": "  C  " ,
+        "KC_D": "  D  " ,
+        "KC_E": "  E  " ,
+        "KC_F": "  F  " ,
+        "KC_G": "  G  " ,
+        "KC_H": "  H  " ,
+        "KC_I": "  I  " ,
+        "KC_J": "  J  " ,
+        "KC_K": "  K  " ,
+        "KC_L": "  L  " ,
+        "KC_M": "  M  " ,
+        "KC_N": "  N  " ,
+        "KC_O": "  O  " ,
+        "KC_P": "  P  " ,
+        "KC_Q": "  Q  " ,
+        "KC_R": "  R  " ,
+        "KC_S": "  S  " ,
+        "KC_T": "  T  " ,
+        "KC_U": "  U  " ,
+        "KC_V": "  V  " ,
+        "KC_W": "  W  " ,
+        "KC_X": "  X  " ,
+        "KC_Y": "  Y  " ,
+        "KC_Z": "  Z  " ,
+        "KC_COMMA": "  ,  ",
+        "KC_COMM": "  ,  ",
+        "KC_DOT": "  .  ",
+        "KC_SCOLON": "  ;  ",
+        "KC_SCLN": "  :  ",
+        "KC_SLASH": "  \/  ",
+        "KC_SLSH": "  \/  ",
+        "KC_BSLS": "  \\  ",
+        "KC_EXLM": "  !  ",
+        "KC_PIPE": "  |  ",
+        "KC_QUOT": "  '  ",
+        "KC_HASH": "  #  ",
+        "KC_AMPR": "  &  ",
+        "KC_PERC": "  %  ",
+        "KC_AT": "  @  ",
+        "KC_DLR": "  $  ",
+        "KC_CIRC": "  ^  ",
+        "KC_EQL": "  =  ",
+        "KC_ASTR": "  *  ",
+        "KC_MINS": "  -  ",
+        "KC_PLUS": "  +  ",
+        "KC_LCBR": "  {  ",
+        "KC_RCBR": "  }  ",
+        "KC_LPRN": "  (  ",
+        "KC_RPRN": "  )  ",
+        "KC_GRV": "  `  ",
+        "KC_LBRC": "  [  ",
+        "KC_RBRC": "  ]  ",
+        "KC_TILD": "  ~  ",
+        "KC_ESC": "  ⎋  " ,
+        "KC_CUT": "  ✄  " ,
+        "KC_UNDO": "  ↶  " ,
+        "KC_REDO": "  ↷  " ,
+        "KC_VOLU": "  🕪  " ,
+        "KC_VOLD": "  🕩  " ,
+        "KC_MUTE":   "  🕨  " ,
+        "KC_TAB": "  ⭾  " ,
+        "KC_MENU": "  𝌆  " ,
+        "KC_CAPSLOCK": "  ⇪  " ,
+        "KC_NUMLK": "  ⇭  " ,
+        "KC_SCRLK": "  ⇳  " ,
+        "KC_PRSCR": "  ⎙  " ,
+        "KC_PAUSE": "  ⎉  " ,
+        "KC_BREAK": "  ⎊  " ,
+        "KC_ENTER": "  ⏎  " ,
+        "KC_BSPACE": "  ⌫  " ,
+        "KC_DELETE": "  ⌦ " ,
+        "KC_INSERT": "  ⎀  " ,
+        "KC_LEFT": "  ◁  " ,
+        "KC_RIGHT": "  ▷  " ,
+        "KC_UP": "  △  " ,
+        "KC_DOWN": "  ▽  " ,
+        "KC_HOME": "  ⇤  " ,
+        "KC_END": "  ⇥  " ,
+        "KC_PGUP": "  ⇞  " ,
+        "KC_PGDOWN": "  ⇟  " ,
+        "KC_LSFT": "  ⇧  " ,
+        "KC_RSFT": "  ⇧  " ,
+        "KC_LCTL": "  ^  " ,
+        "KC_RCTL": "  ^  " ,
+        "KC_LALT": "  ⎇  " ,
+        "KC_RALT": "  ⎇  " ,
+        "KC_HYPER": "  ✧  " ,
+        "KC_LGUI": "  ⌘  " ,
+        "KC_RGUI": "  ⌘  "
+    }
+}
+*/
+
